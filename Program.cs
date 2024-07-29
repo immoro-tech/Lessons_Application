e93d60ab176b4171c93867bfa7fe11ff5973e337
Kyrylo Fedorenko <fedorenkokirill71@gmail.com>
2024-07-29 19:04:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var tcpListener = new TcpListener(IPAddress.Any, 8888);
+try
+{
+    tcpListener.Start();
+    Console.WriteLine("Server started");
+
+    while (true)
+    {
+        using var tcpClient = await tcpListener.AcceptTcpClientAsync();
+        Console.WriteLine(tcpClient.Client.RemoteEndPoint);
+    }
+}
+finally
+{
+    tcpListener.Stop();
+    Console.WriteLine("Connect closed");
+}
